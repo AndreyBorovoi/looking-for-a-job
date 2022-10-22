ed50717c57bb9a4349e3aa547444122df287f85e
Andrey <andreyborovoi@mail.ru>
2022-10-22 18:31:33 +0300

    add fade in About

--- components/mainPage/About/About.tsx
+++ components/mainPage/About/About.tsx
@@ -1,5 +1,8 @@
+import { ReactNode, useEffect, useState } from 'react';
+
 import Container from '@mui/material/Container';
-import { ReactNode } from 'react';
+import Fade from '@mui/material/Fade';
+
 import styles from './about.module.scss';
 
 type PointType = {
@@ -31,28 +34,48 @@ const points: PointType[] = [
 ];
 
 export const About = () => {
+  const [visibility, setVisibility] = useState(false);
+
+  useEffect(() => {
+    let options = {
+      root: null,
+      threshold: 0.5,
+    };
+
+    const callback = (entries: any, observer: any) => {
+      if (entries[0].isIntersecting) {
+        setVisibility(true);
+      }
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(document.getElementById('About') as Element);
+  }, []);
+
   return (
-    <section className={styles.section}>
-      <Container fixed className={styles.container}>
-        <div className={styles.title}>
-          <div className={styles.tag}>Опыт</div>
-          <div className={styles.titleText}>
-            Обладаю значительным опытом, который помогает мне выполнять
-            поставленные бизнес задачи
+    <section id="About" className={styles.section}>
+      <Fade in={visibility} timeout={1500}>
+        <Container fixed className={styles.container}>
+          <div className={styles.title}>
+            <div className={styles.tag}>Опыт</div>
+            <div className={styles.titleText}>
+              Обладаю значительным опытом, который помогает мне выполнять
+              поставленные бизнес задачи
+            </div>
           </div>
-        </div>
-        <div className={styles.points}>
-          {points.map(({ key, number, text, description }: PointType) => {
-            return (
-              <div key={key} className={styles.point}>
-                <div className={styles.pointNumber}>{number}</div>
-                <div className={styles.pointText}>{text}</div>
-                <div className={styles.pointDescription}>{description}</div>
-              </div>
-            );
-          })}
-        </div>
-      </Container>
+          <div className={styles.points}>
+            {points.map(({ key, number, text, description }: PointType) => {
+              return (
+                <div key={key} className={styles.point}>
+                  <div className={styles.pointNumber}>{number}</div>
+                  <div className={styles.pointText}>{text}</div>
+                  <div className={styles.pointDescription}>{description}</div>
+                </div>
+              );
+            })}
+          </div>
+        </Container>
+      </Fade>
     </section>
   );
 };
