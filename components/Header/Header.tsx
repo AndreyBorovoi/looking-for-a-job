fede28db2844797f1d03c8420fbf90d2fbb8ac40
Andrey <andreyborovoi@mail.ru>
2022-10-22 13:43:37 +0300

    add Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,12 +2,36 @@ import Container from "@mui/material/Container";
 import { useContext } from "react";
 import { LanguageContext } from "../Language";
 
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+import { Logo } from "./Logo";
+import styles from "./header.module.scss";
+
 export function Header() {
   const languageContext = useContext(LanguageContext);
 
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    languageContext?.changeLanguage(event.target.value);
+  };
+
   return (
-    <Container fixed>
-      <div>{languageContext?.language}</div>
+    <Container fixed className={styles.container}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <FormControl sx={{ minWidth: "70px" }} size="small">
+        <Select
+          value={languageContext?.language}
+          onChange={handleChange}
+          variant="standard"
+          className={styles.select}
+        >
+          <MenuItem value={"en"}>English</MenuItem>
+          <MenuItem value={"ru"}>Русский</MenuItem>
+        </Select>
+      </FormControl>
     </Container>
   );
 }
--- components/Language/Language.tsx
+++ components/Language/Language.tsx
@@ -1,15 +1,9 @@
-import {
-  ReactNode,
-  createContext,
-  useState,
-  Dispatch,
-  SetStateAction,
-} from "react";
+import { ReactNode, createContext, useState } from "react";
 
 type Language = "en" | "ru";
 export type ContextType = {
   language: Language;
-  setLanguage: Dispatch<SetStateAction<Language>>;
+  changeLanguage: (value: string) => void;
 } | null;
 
 export const LanguageContext = createContext<ContextType>(null);
@@ -17,8 +11,23 @@ export const LanguageContext = createContext<ContextType>(null);
 export function Language({ children }: { children: ReactNode }) {
   const [language, setLanguage] = useState<Language>("en");
 
+  const changeLanguage = (value: string) => {
+    switch (value) {
+      case "en":
+        setLanguage("en");
+        break;
+
+      case "ru":
+        setLanguage("ru");
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
-    <LanguageContext.Provider value={{ language, setLanguage }}>
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
       {children}
     </LanguageContext.Provider>
   );
