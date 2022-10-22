e9abce55d3a960d39ef21df730a2aa49d1c9d3b0
Andrey <andreyborovoi@mail.ru>
2022-10-22 15:01:36 +0300

    add Banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next';
 import { Header } from '../components/Header';
+import { Banner } from '../components/mainPage/Banner';
 
 const Home: NextPage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
 };
 
 export default Home;
