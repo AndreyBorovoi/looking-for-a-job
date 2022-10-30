1e8c14656c453e2e5229a95bd2f16756cc8eaf7a
Andrey <andreyborovoi@mail.ru>
2022-10-30 20:43:07 +0300

    move Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Header } from '../components/Header';
+import { Header } from '../components/mainPage/Header';
 import { Banner } from '../components/mainPage/Banner';
 import { About } from '../components/mainPage/About';
 import { Skills } from '../components/mainPage/Skills';
