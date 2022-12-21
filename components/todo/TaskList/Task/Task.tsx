30bb2907fc81ab1c85c69d263a0a6f29711e79de
Andrey <andreyborovoi@mail.ru>
2022-12-21 20:41:19 +0300

    add Task

--- components/todo/TaskList/Task/Task.tsx
+++ components/todo/TaskList/Task/Task.tsx
@@ -1,5 +1,107 @@
+import { ChangeEventHandler, useState } from 'react';
+
 import { Task as TaskProps } from '../../store/taskListSlice';
 
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import BackspaceIcon from '@mui/icons-material/Backspace';
+
+import { styled } from '@mui/material/styles';
+
+import { useAppDispatch } from '../../store/hooks';
+import {
+  changeStatus,
+  changeTitle,
+  deleteTask,
+} from '../../store/taskListSlice';
+
+const StyledTask = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  paddingLeft: '24px',
+  paddingRight: '24px',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  marginBottom: '10px',
+  width: '100%',
+}));
+
+const StyledTaskName = styled(Button)(({ theme }) => ({
+  flexGrow: 1,
+  textAlign: 'left',
+  overflow: 'hidden',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+}));
+
+const StyledCheckbox = styled(Checkbox)(({ theme }) => ({
+  marginRight: '20px',
+}));
+
+const StyledNameInput = styled(TextField)(({ theme }) => ({
+  flexGrow: 1,
+}));
+
+const StyledDeleteIcon = styled(BackspaceIcon)(({ theme }) => ({
+  justifySelf: 'flex-end',
+  marginLeft: '30px',
+}));
+
 export const Task = ({ id, projectId, isDone, title }: TaskProps) => {
-  return <div>{title}</div>;
+  const dispatch = useAppDispatch();
+
+  const [isInputShowed, setIsInputShowed] = useState(false);
+  const [newName, setNewName] = useState(title);
+
+  const onCheckboxClick = () => {
+    dispatch(changeStatus({ id: id }));
+  };
+
+  const onNameInputBlur = () => {
+    dispatch(changeTitle({ id: id, title: newName }));
+    setIsInputShowed(false);
+  };
+
+  const focusOnInput: (instance: HTMLDivElement | null) => void = (
+    instance
+  ) => {
+    const inputs = instance?.getElementsByTagName('input');
+
+    if (inputs && inputs[0]) {
+      inputs[0].focus();
+    }
+  };
+
+  const onTitleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const onDeleteTask = () => {
+    dispatch(deleteTask({ id: id }));
+  };
+
+  return (
+    <StyledTask>
+      <StyledCheckbox checked={isDone} onClick={onCheckboxClick} />
+      {isInputShowed ? (
+        <StyledNameInput
+          variant="standard"
+          onBlur={onNameInputBlur}
+          ref={focusOnInput}
+          value={newName}
+          onChange={onTitleChange}
+        />
+      ) : (
+        <StyledTaskName onClick={() => setIsInputShowed(true)}>
+          {title}
+        </StyledTaskName>
+      )}
+      <StyledDeleteIcon onClick={onDeleteTask} />
+    </StyledTask>
+  );
 };
--- components/todo/store/taskListSlice.ts
+++ components/todo/store/taskListSlice.ts
@@ -12,6 +12,9 @@ export type Task = {
 };
 
 type AddNewTaskAction = Pick<Task, 'projectId'>;
+type ChangeStatusAction = Pick<Task, 'id'>;
+type DeleteTaskAction = Pick<Task, 'id'>;
+type ChangeTitleAction = Pick<Task, 'id' | 'title'>;
 
 export const taskListSlice = createSlice({
   name: 'taskList',
@@ -24,8 +27,8 @@ export const taskListSlice = createSlice({
   },
   reducers: {
     addNewTask: (state, action: PayloadAction<AddNewTaskAction>) => {
-      const newId =
-        Math.max(...state.value.taskList.map((task) => task.id)) + 1;
+      const ids = state.value.taskList.map((task) => task.id);
+      const newId = ids.length > 0 ? Math.max(...ids) + 1 : 0;
       const newTask = {
         id: newId,
         isDone: false,
@@ -34,10 +37,27 @@ export const taskListSlice = createSlice({
       };
       state.value.taskList.push(newTask);
     },
-    deleteTask: (state) => {},
+    deleteTask: (state, action: PayloadAction<DeleteTaskAction>) => {
+      state.value.taskList = state.value.taskList.filter(
+        (v) => v.id !== action.payload.id
+      );
+    },
+    changeStatus: (state, action: PayloadAction<ChangeStatusAction>) => {
+      const task = state.value.taskList.find((v) => v.id === action.payload.id);
+      if (task) {
+        task.isDone = !task.isDone;
+      }
+    },
+    changeTitle: (state, action: PayloadAction<ChangeTitleAction>) => {
+      const task = state.value.taskList.find((v) => v.id === action.payload.id);
+      if (task) {
+        task.title = action.payload.title;
+      }
+    },
   },
 });
 
-export const { addNewTask, deleteTask } = taskListSlice.actions;
+export const { addNewTask, deleteTask, changeStatus, changeTitle } =
+  taskListSlice.actions;
 
 export default taskListSlice.reducer;
